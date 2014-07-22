81667ac3eebbf9f59a77c0470bc5854ea0de1fab
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-07-22 19:08:19 +0900

    クイズ追加ダイアログ作成から

---
+++ Assets/QuizBox/Scripts/Top/QuizTopicInitializer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuizTopicInitializer : MonoBehaviour {
+
+	public GameObject topCellPrefab;
+	public UIGrid grid;
+	
+	void Start () {
+		IList<IDictionary> quizList = QuizListDao.instance.GetQuizList ();
+		foreach (IDictionary quiz in quizList) {
+			GameObject cellObject = Instantiate (topCellPrefab) as GameObject;
+			grid.AddChild (cellObject.transform);
+			cellObject.transform.localScale = new Vector2 (1f, 1f);
+			int id = (int)quiz [QuizListDao.ID_FIELD];
+			string name = (string)quiz [QuizListDao.TITLE_FIELD];
+			string quizUrl = (string)quiz [QuizListDao.QUIZ_URL_FIELD];
+			CellTop cellTop = cellObject.GetComponent<CellTop> ();
+			cellTop.id = id;
+			cellTop.name = name;
+			cellTop.quizUrl = quizUrl;
+		}
+	}
+}
