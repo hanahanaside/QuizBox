64810936d38e3841461fd4d7536c2a7e281d6100
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-07-22 15:11:25 +0900

    画像変更開始

--- Assets/QuizBox/Scripts/Result/ResultInitializer.cs
+++ Assets/QuizBox/Scripts/Result/ResultInitializer.cs
@@ -3,14 +3,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class ResultInitializer : MonoBehaviour
-{
+public class ResultInitializer : MonoBehaviour {
 
 	public UILabel resultLabel;
 
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		int score = ScoreKeeper.instance.score;
 		int size = QuizListManager.instance.quizList.Count;
 		string result = size + "問中" + score + "問正解!!";
@@ -22,6 +20,13 @@ public class ResultInitializer : MonoBehaviour
 		historyData.mode = QuizListManager.instance.modeName;
 	
 		HistoryDataDao.instance.InsertHistoryData (historyData);
+
+		System.Text.StringBuilder sb = new System.Text.StringBuilder ();
+		sb.Append ("result = " + result + "\n");
+		sb.Append ("date = " + DateTime.Now.ToString ());
+		sb.Append ("title = " + SelectedQuiz.instance.name + "\n");
+		sb.Append ("mode = " + QuizListManager.instance.modeName);
+		resultLabel.text = sb.ToString();
 	}
 	
 }
