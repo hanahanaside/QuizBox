32c03c2661ed41d8b0acc724dd3930b6297d5c7f
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-09-09 16:28:44 +0900

    更にミス

--- Assets/QuizBox/Scripts/Top/FooterPositionSetter.cs
+++ Assets/QuizBox/Scripts/Top/FooterPositionSetter.cs
@@ -8,12 +8,11 @@ public class FooterPositionSetter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 #if UNITY_IPHONE
-		string generationName = iPhone.generation.ToString();
-		Debug.Log("generation = "+ generationName);
-		if(generationName == "iPhone4S" || generationName == "iPhone4"){
-			uiAnchor.relativeOffset.Set(0,0.145f);
+		if(Screen.height == 960){
+		uiAnchor.relativeOffset.Set(0,0.145f);
 		}
 #endif
+
 
 		#if UNITY_ANDROID
 
