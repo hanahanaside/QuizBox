af2a56f87eee8b50a9e9e0e5b18cf7a3b2c90668
hanahanaside <hanada@hanauta-no-Mac-mini.local>
2014-11-17 14:40:18 +0900

    中断

--- Assets/QuizBox/Scripts/Top/QuizTopicInitializer.cs
+++ Assets/QuizBox/Scripts/Top/QuizTopicInitializer.cs
@@ -20,9 +20,9 @@ public class QuizTopicInitializer : MonoBehaviour {
 		unlockLimit = 1;
 		#endif
 		if (timeSpan.TotalDays >= unlockLimit) {
-			GameObject incentiveButtonObject = Instantiate (incentiveButtonPrefab) as GameObject;
-			grid.AddChild (incentiveButtonObject.transform);
-			incentiveButtonObject.transform.localScale = new Vector2 (1f,1f);
+	//		GameObject incentiveButtonObject = Instantiate (incentiveButtonPrefab) as GameObject;
+	//		grid.AddChild (incentiveButtonObject.transform);
+	//		incentiveButtonObject.transform.localScale = new Vector2 (1f,1f);
 		} 
 		List<Quiz> quizList = QuizListDao.instance.GetQuizList ();
 		foreach (Quiz quiz in quizList) {
